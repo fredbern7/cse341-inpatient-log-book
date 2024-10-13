db208a24bd88fb20cd0d612dcad50294907e6f89
Berny <fredbern7@gmail.com>
2024-10-13 16:01:17 +1100

    update oath

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app
 passport.use(new GitHubStrategy( {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.callbackURL
+    callbackURL: process.env.CALLBACK_URL
 },
 function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
